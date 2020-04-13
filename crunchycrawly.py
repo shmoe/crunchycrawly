7f2daedc5052296f7f75f4f24b849146cbd25c4b
Griffin <griffinnelson@outlook.com>
2020-04-13 16:54:56 -0400

    adjusted default profile directory, assumes only one *.default-release directory exists in a single Firefox install

--- crunchycrawly.py
+++ crunchycrawly.py
@@ -1,6 +1,7 @@
 import crunchyroll_utils as cr
 import sqlite3
 import cursor
+
 import sys
 import time
 import math
@@ -107,23 +108,23 @@ def progressBar(completed_tasks_ref, total_tasks):
 if __name__ == "__main__":
 	import concurrent.futures
 	import os.path
+	import glob
+	#will break on implementing commandline options
+	if len(sys.argv) > 1:
+		profile = sys.argv[1]
+	else:
+		profile = "*.default-release"
 
 	#TODO implement for non-Windows
 	if os.name == "nt":
-		profile_path = os.path.expandvars("%APPDATA%\\Mozilla\\Firefox\\Profiles\\")
-		if len(sys.argv) > 1:
-			profile_path += sys.argv[1] + "\\" # will break on implementing commandline options
-		else:
-			profile_path += "rblyzgbi.default-release\\"
+		profile_path = os.path.expandvars("%APPDATA%\\Mozilla\\Firefox\\Profiles\\" + profile + "\\")
 	elif os.name == "posix":
-		profile_path = os.path.expandvars("~/.mozilla/firefox/") #at least for Debian. Test?
-
+		profile_path = os.path.expandvars("~/.mozilla/firefox/" + profile + "/") #test for macOS
 		if os.getenv("DEBUG", default=False):
-			profile_path = "testenv/" # debug
-
+			profile_path = "testenv/" #debug
 
 	try:
-		bookmarks = parseBookmarks(profile_path + "places.sqlite")
+		bookmarks = parseBookmarks(glob.glob(profile_path)[0] + "places.sqlite")
 	except Exception as e:
 		sys.exit(e)
 
